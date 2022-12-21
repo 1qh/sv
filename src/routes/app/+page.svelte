9a219a3c6e21c1703110fb800bd547848d919739
Huy <laiquanghuy24122001@gmail.com>
2022-12-21 21:49:53 +0700

    .

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,21 +1,46 @@
 <script lang="ts">
-	import { Button, Hr, Layout, Li, List, Listgroup, P } from 'flowbite-svelte'
+	import {
+		A,
+		Button,
+		ButtonGroup,
+		Checkbox,
+		FloatingLabelInput,
+		Hr,
+		Img,
+		Li,
+		List,
+		Listgroup,
+		ListgroupItem,
+		NumberInput,
+		P,
+		Range
+	} from 'flowbite-svelte'
 	import Car from './car.svelte'
 	import Greet from './greet.svelte'
 	import Prices from './prices.svelte'
-	const car = {
-		name: 'VFe34',
-		type: 'SUV',
-		brand: 'Vinfast'
-	}
-	type User = {
-		login: boolean
-	}
-	let user: User = {
-		login: false
-	}
-	let toggle = () => (user.login = !user.login)
+	import { elapsed, time } from './stores.js'
 
+	const formatter = new Intl.DateTimeFormat('en', {
+		hour12: true,
+		hour: 'numeric',
+		minute: '2-digit',
+		second: '2-digit'
+	})
+	let yes: boolean = false
+	let toggle = () => (yes = !yes)
+
+	type Car = {
+		name: string
+		type: string
+		brand: string
+	}
+	let mycar: Car = {
+		name: '',
+		type: '',
+		brand: ''
+	}
+
+	let stuff: string[] = ['table', 'chair', 'computer', 'mouse', 'keyboard']
 	let cart = [
 		{ id: 1, name: 'imac' },
 		{ id: 2, name: 'iphone' },
@@ -50,11 +75,30 @@
 	}
 	let promise = fetchRandom()
 	let clickForRandom = () => (promise = fetchRandom())
+
+	type Coordinate = {
+		x: number
+		y: number
+	}
+	let mouse: Coordinate = {
+		x: 0,
+		y: 0
+	}
+
+	const fetchMeme = (async () => {
+		const response = await fetch('https://meme-api.com/gimme')
+		return await response.json()
+	})()
 </script>
 
 <div>
+	<p>
+		{formatter.format($time)}
+		this page has been open for {$elapsed} seconds
+	</p>
+
 	<P align="center">
-		{#if user.login}
+		{#if yes}
 			<Button
 				pill
 				gradient
@@ -67,16 +111,20 @@
 			<Button
 				pill
 				gradient
-				color="purpleToBlue"
-				shadow="purple"
+				color="greenToBlue"
+				shadow="green"
 				size="lg"
 				class="w-24"
 				on:click={toggle}>Log in</Button>
 		{/if}
 	</P>
-	<Hr />
-
-	<P space="widest" size="xl" weight="extrabold"><Car {...car} /></P>
+	<Checkbox bind:checked={yes}>
+		{#if yes}
+			Thank you!
+		{:else}
+			Check this please
+		{/if}
+	</Checkbox>
 
 	<Hr />
 
@@ -84,16 +132,32 @@
 	<Greet holiday={'New Year'} />
 
 	<Hr />
-	<Layout gap={0}>
-		<P align="center">
-			<Button class="w-48" on:click={removeFirst}>Remove first</Button>
-		</P>
-		<P align="center">
-			<Button class="w-48" on:click={removeLast}>Remove last</Button>
-		</P>
-	</Layout>
+	<ButtonGroup>
+		<FloatingLabelInput style="outlined" label="Car name" bind:value={mycar.name} />
+		<FloatingLabelInput style="outlined" label="Car brand" bind:value={mycar.brand} />
+		<FloatingLabelInput style="outlined" label="Car type" bind:value={mycar.type} />
+	</ButtonGroup>
+	<P space="widest" size="xl" weight="extrabold"><Car {...mycar} /></P>
 
-	<Listgroup items={cart} let:item><Prices product={item.name} /></Listgroup>
+	<Hr />
+	<div class="grid justify-center">
+		<Listgroup active items={stuff} let:item class="w-24">
+			{item}
+		</Listgroup>
+	</div>
+
+	<ButtonGroup>
+		<Button on:click={removeFirst}>Remove first</Button>
+		<Button on:click={removeLast}>Remove last</Button>
+	</ButtonGroup>
+
+	<Listgroup class="w-48">
+		{#each cart as item (item.id)}
+			<ListgroupItem>
+				<Prices product={item.name} />
+			</ListgroupItem>
+		{/each}
+	</Listgroup>
 
 	<List tag="ul">
 		{#each cart as item (item.id)}
@@ -114,13 +178,9 @@
 		&#8730;{num} = {sqr}
 	</Button>
 
-	<Hr />
-
 	<Button on:click={appendNum}>
 		{arr.join(' + ')} = {sum}
 	</Button>
-
-	<Hr />
 
 	<Button on:click={clickForRandom}>Generate random number</Button>
 	{#await promise}
@@ -132,4 +192,35 @@
 	{/await}
 
 	<Hr />
+
+	<div
+		class="h-screen grid justify-center items-center"
+		on:mousemove={e => {
+			mouse = { x: e.clientX, y: e.clientY }
+		}}>
+		<div>
+			Cursor at
+			<ButtonGroup divClass="flex">
+				<NumberInput class="text-center" bind:value={mouse.x} />
+				<NumberInput class="text-center" bind:value={mouse.y} />
+			</ButtonGroup>
+			<Range max="1920" bind:value={mouse.x} />
+			<Range max="1080" bind:value={mouse.y} />
+		</div>
+	</div>
+
+	<div class="grid justify-center">
+		{#await fetchMeme}
+			<p>...waiting</p>
+		{:then data}
+			<A href={data.postLink}>{data.postLink}</A>
+			<Img
+				src={data.url}
+				caption={data.title}
+				class="rounded-3xl transition-all duration-500 blur-lg hover:blur-none grayscale hover:grayscale-0"
+				alignment="mx-auto" />
+		{:catch error}
+			<p>An error occurred!</p>
+		{/await}
+	</div>
 </div>
